d40d19d0fa86e8638fe6281bda4043bb098b6d59
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2022-12-06 03:22:41 +0100

    feat: make the app useable via a cli

--- soundpad-bumblebee/src/main.rs
+++ soundpad-bumblebee/src/main.rs
@@ -1,12 +1,20 @@
-use color_eyre::eyre::Result;
+use clap::Parser;
+use color_eyre::eyre::{eyre, Result};
 use soundpad_remote_client::ClientBuilder;
 use std::time::Duration;
 use tracing::{info, metadata::LevelFilter};
 use tracing_error::ErrorLayer;
 use tracing_subscriber::{fmt::format, prelude::*};
 
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Args {
+    message: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let args = Args::parse();
     let registry = tracing_subscriber::registry();
     #[cfg(feature = "console")]
     {
@@ -32,13 +40,11 @@ async fn main() -> Result<()> {
 
     let sounds = client.get_sound_list().await?;
 
-    let esel = sounds.iter().find(|&s| s.title.contains("Esel")).unwrap();
+    let sound = sounds
+        .iter()
+        .find(|&s| s.title.contains(&args.message))
+        .ok_or(eyre!("Could not find a sound containing {}", args.message))?;
 
-    client.play_sound(esel).await?;
-    client.play_sound(esel).await?;
-    client.play_sound(esel).await?;
-    client
-        .custom_command("DolaySound(1)", client.debounce)
-        .await?;
+    client.play_sound(sound).await?;
     Ok(())
 }
